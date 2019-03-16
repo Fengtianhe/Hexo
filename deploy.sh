d2b432688874f0e0e1a694a6f1ea6032ba4a9f7c
fengtianhe <545704061@qq.com>
2019-03-16 13:24:32 +0800

    deploy shell

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 hexo clean
 hexo g -d
-ssh fengtianhe@120.27.127.94 "cd /home/fengtianhe/blog && git reset --hard HEAD && git pull"
+ssh fengtianhe@120.27.127.94 "cd /home/fengtianhe && rm -rf blog && git clone https://git.dev.tencent.com/fengtianhe/blog.git"
