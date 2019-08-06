981d40294f0d9033d506de4fd1ad4dc591b3e58e
fengtianhe <545704061@qq.com>
2019-08-06 18:13:41 +0800

    fix tag

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 hexo clean
 hexo g -d
-ssh fengtianhe@120.27.127.94 "cd /home/fengtianhe && rm -rf blog && git clone https://git.dev.tencent.com/fengtianhe/blog.git"
+ssh fengtianhe@120.27.127.94 "cd /home/fengtianhe/Hexo && git pull"
